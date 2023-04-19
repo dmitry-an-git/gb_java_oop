41970a1267d0c39c411a85b06416ccde73f65d54
Dmitry An <dmitry.andreyev@gmail.com>
2023-04-19 12:36:55 +0400

    first seminar

---
+++ seminar01/Chocolate.java
@@ -0,0 +1,26 @@
+public class Chocolate extends Product {
+    int calories;
+
+    public void setCalories(int calories) {
+        if (calories <= 0){
+            throw new RuntimeException("Калорийность указана некорректно.");
+        }
+        this.calories = calories;
+    }
+
+    public Chocolate(String name, int price, int calories){
+        super(name,price);
+        setCalories(calories);
+    }
+
+    @Override
+    public String displayInfo() {
+        // TODO Auto-generated method stub
+        return String.format("%s - ккал: %d", super.displayInfo(), calories);
+    }
+
+    @Override
+    public String toString() {
+        return displayInfo();
+    }
+}
---
+++ seminar01/VendingMachine.java
@@ -0,0 +1,91 @@
+/**
+ * Автомат
+ */
+
+import java.util.List;
+
+public class VendingMachine {
+
+    private final List<Product> products;
+    private double cash;
+
+    {
+        cash = 0;
+    }
+
+    public VendingMachine(List<Product> products){
+        this.products = products;
+    }
+
+    public void depositCash(double price){
+        this.cash += price;
+    }
+
+    public void tellCash(){
+        System.out.println(String.format("В автомате денег: %.2f", this.cash));
+    }
+
+    public void tellValue(){
+        double value = 0;
+        for (Product product : products) {
+            value += product.getPrice();
+        }
+        System.out.println(String.format("В автомате продукции на: %.2f", value));
+    }
+
+    public Product buyProduct(Class<?> cls, Wallet wallet){
+        boolean present = false;
+        for (Product product : products){
+            if (cls.isInstance(product)) {
+                present = true;
+                if (wallet.flushBalance(product.getPrice())) {
+                    this.depositCash(product.getPrice());
+                    this.products.remove(product);
+                    System.out.println("Вы купили: ");
+                    System.out.println(product.displayInfo());
+                    return product;
+                }   
+            }
+        }
+        if (present == true) {
+            System.out.println("Недостаточно денег!");
+        } else {
+            System.out.println("Таких продуктов больше нет!");
+        }
+        return null;
+    }
+
+
+    public BottleOfWater getBottleOfWater(String name, int volume){
+
+        for (Product product : products){
+            if (product instanceof BottleOfWater){
+                if (product.getName() == name && ((BottleOfWater)product).getVolume() == volume){
+                    System.out.println("Вы купили воду: ");
+                    System.out.println(product.displayInfo());
+                    return (BottleOfWater)product;
+                }
+            }
+        }
+        System.out.println("Такой бутылки с водой нет в автомате.");
+        return null;
+    }
+
+    public BottleOfWater getBottleOfMilk(String name, int volume){
+
+        for (Product product : products){
+            if (product instanceof BottleOfWater){
+                if (product.getName() == name && ((BottleOfWater)product).getVolume() == volume){
+                    System.out.println("Вы купили молоко: ");
+                    System.out.println(product.displayInfo());
+                    return (BottleOfWater)product;
+                }
+            }
+        }
+        System.out.println("Такой бутылки с молоком нет в автомате.");
+        return null;
+    }
+
+   
+
+}
