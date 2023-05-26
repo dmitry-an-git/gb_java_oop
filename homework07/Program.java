7b4b30b5995b9d932df94ffcf14fcd75d04aa776
Dmitry An <dmitry.andreyev@gmail.com>
2023-05-26 14:57:39 +0400

    homework07 upd

--- homework07/Program.java
+++ homework07/Program.java
@@ -14,11 +14,12 @@ public class Program {
      */
     public static void main(String[] args) {
 
-        MktCEO mktCEO = new MktCEO();
+        // here we define a universe of job positions 
+        // each instance contains a title and range of salaries
+        MktCEO mktCEO = new MktCEO(); 
         MktMaster mktMaster = new MktMaster();
         MktTrainee mktTrainee = new MktTrainee();
-
-        Mkt jobMarket = new Mkt();
+        Mkt jobMarket = new Mkt(); // just a list with an imprint of current job market
         jobMarket.addPosition(mktCEO);
         jobMarket.addPosition(mktMaster);
         jobMarket.addPosition(mktTrainee);
@@ -28,9 +29,9 @@ public class Program {
         Company google = new Company("Google", jobAgency, jobMarket);
         Company yandex = new Company("Yandex", jobAgency, jobMarket);
 
-        Master ivanov = new Master("Ivanov", 5);
-        Master sidorov = new Master("Sidorov", 5);
-        Student petrov = new Student("Petrov", 5);
+        Master ivanov = new Master("Ivanov", 5); // the second parameter is the skill level
+        Master sidorov = new Master("Sidorov", 4);
+        Student petrov = new Student("Petrov", 6);
         CEO smirnov = new CEO("Smirnov", 5);
         Candidate<MktTrainee> andreev = new Candidate<>("Andreev", mktTrainee, 10000, 5); //parametrized
 
